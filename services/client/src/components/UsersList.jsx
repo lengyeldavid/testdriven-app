8eb2ab323c853e74f280ba78c54c8798862d3117
David Lengyel <lengyeldvid@gmail.com>
2019-04-28 21:27:11 +0200

    Add initial code for React.js client, enable Flask debug toolbar

---
+++ services/client/src/components/UsersList.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const UserList = (props) => {
+  return (
+    <div>
+      {
+        props.users.map((user) => {
+          return (
+            <h4
+              key={user.id}
+              className="box title is-4"
+            >{ user.username }
+            </h4>
+          )
+        })
+      }
+    </div>
+  )
+};
+
+export default UserList;
